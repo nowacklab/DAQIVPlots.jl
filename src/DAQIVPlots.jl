35fbc1d1469174f13195a3297eca5a31c37a0a06
Alex Striff <abs299@cornell.edu>
2024-01-24 16:59:27 -0500

    Fix PDF scaling; start mmap implementation

--- src/DAQIVPlots.jl
+++ src/DAQIVPlots.jl
@@ -1,5 +1,6 @@
 module DAQIVPlots
 
+using Mmap
 using StaticArrays
 using JSON
 using NPZ
@@ -59,11 +60,13 @@ function main(dn = 0, plot = true, savefig = true)
 
     preamp_gain = parameters["preamp"]["gain"]
     incoeff = in_coeff(parameters["daqiv"]["daqio"]["input"]["calibration"]["coefficients"])
-    in_samples = reinterpret(Int16, read(joinpath(recent, "input-samples.bin")))
+    #in_samples = reinterpret(Int16, read(joinpath(recent, "input-samples.bin")))
+    in_stream = open(joinpath(recent, "input-samples.bin"), "r")
+    in_samples = mmap(in_stream, Vector{Int16})
     in_voltages = in_voltage.(incoeff, in_samples) ./ preamp_gain .* u"V"
 
-    resistance = mean(abs.(in_voltages[1:length(out_currents)] ./ out_currents)) |> u"Ω"
-    println("Resistance: $(resistance)")
+    # resistance = mean(abs.(in_voltages[1:length(out_currents)] ./ out_currents)) |> u"Ω"
+    # println("Resistance: $(resistance)")
 
     sweeps = parameters["daqiv"]["daqio"]["output"]["signal"]["regenerations"]
     quarter_sweep = div(length(out_samples), 4)
@@ -79,8 +82,6 @@ function main(dn = 0, plot = true, savefig = true)
     negative_transitions = zeros(Int, quarter_sweep)
     negative_transition_currents = zeros(eltype(out_currents), sweeps)
     missing_negative_transitions = 0
-
-    println(sweeps)
 
     transition_threshold = 5.0u"mV"
     for sweep in 1:sweeps
@@ -105,6 +106,7 @@ function main(dn = 0, plot = true, savefig = true)
         end
     end
 
+    close(in_stream)
 
     if plot
         min_transition = findfirst(positive_transitions .> 0)
@@ -112,19 +114,18 @@ function main(dn = 0, plot = true, savefig = true)
         span = max_transition - min_transition
         margin = div(span, 8)
         plot_indices = (min_transition - margin):(max_transition + margin)
+	dI = mean(out_currents[positive_offset .+ plot_indices[2:end]] .- out_currents[positive_offset .+ plot_indices[1:end-1]])
         fig, ax, plt = lines(
-            ustrip.(u"μA", out_currents[plot_indices .+ positive_offset]),
-            positive_transitions[plot_indices],
+	    ustrip.(u"μA", out_currents[plot_indices .+ positive_offset]),
+	    positive_transitions[plot_indices] ./ (ustrip(u"μA", dI) * sweeps),
             )
         ax.xlabel = rich("I = V / R", subscript("b"), " (μA)")
-        ax.ylabel = "Transitions"
+	ax.ylabel = rich("Transition probability density (μA", superscript("-1"), ")")
         ax.title = "$(recent)"
 
-        # TODO: KDE not verified to have correct scaling
-        dI = mean(out_currents[positive_indices[2:end]] .- out_currents[positive_indices[1:end-1]])
         positive_kde = kde_lscv(ustrip.(u"μA", positive_transition_currents))
         #positive_kde = kde(ustrip.(u"μA", positive_transition_currents), bandwidth = 1e-1)
-        lines!(ax, positive_kde.x, positive_kde.density / ustrip(u"μA", dI))
+	lines!(ax, positive_kde.x, positive_kde.density)
         
         #=
         fig, ax, plt = lines(
@@ -140,9 +141,9 @@ function main(dn = 0, plot = true, savefig = true)
         if savefig
             save("../plots/$(recent).png", fig, px_per_unit = 4)
         end
-        return parameters, (fig, ax, plt)
+        #return parameters, (fig, ax, plt)
     end
-    return parameters, nothing
+    return parameters, positive_transitions, positive_kde, dI
 end
 
 export main
