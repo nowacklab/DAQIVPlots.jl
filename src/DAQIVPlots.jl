f83b649611e8e07d719114291154571665cb7ca0
Alex Striff <abs299@cornell.edu>
2024-01-24 17:36:10 -0500

    Mmap implementation and thresholding based on samples

--- src/DAQIVPlots.jl
+++ src/DAQIVPlots.jl
@@ -4,6 +4,7 @@ using Mmap
 using StaticArrays
 using JSON
 using NPZ
+using Roots
 using GLMakie
 using Unitful
 using Statistics
@@ -23,7 +24,7 @@ buffer(c::OutInverseCoefficients) = c.c
 
 inv(c::OutCoefficients) = OutInverseCoefficients([-c.c[1], one(eltype(c.c))] ./ c.c[2])
 out_inv_coeff(c) = Ref(buffer(inv(OutCoefficients(c))))
-out_voltage(c, sample::Int16) = c[1] + c[2] * sample
+out_voltage(c, sample) = c[1] + c[2] * sample
 
 struct InCoefficients
     c::SVector{4, Float64}
@@ -31,8 +32,8 @@ end
 
 buffer(c::InCoefficients) = c.c
 
-in_coeff(c) = Ref(buffer(InCoefficients(c)))
-in_voltage(c, sample::Int16) = c[1] + c[2] * sample + c[3] * sample^2 + c[4] * sample^3
+in_coeff(c) = buffer(InCoefficients(c))
+in_voltage(c, sample) = c[1] + c[2] * sample + c[3] * sample^2 + c[4] * sample^3
 
 function main(dn = 0, plot = true, savefig = true)
     dirs = sort([dir for dir in readdir(sort = false) if startswith(dir, "daqiv-")], rev = true)
@@ -60,33 +61,32 @@ function main(dn = 0, plot = true, savefig = true)
 
     preamp_gain = parameters["preamp"]["gain"]
     incoeff = in_coeff(parameters["daqiv"]["daqio"]["input"]["calibration"]["coefficients"])
-    #in_samples = reinterpret(Int16, read(joinpath(recent, "input-samples.bin")))
     in_stream = open(joinpath(recent, "input-samples.bin"), "r")
     in_samples = mmap(in_stream, Vector{Int16})
-    in_voltages = in_voltage.(incoeff, in_samples) ./ preamp_gain .* u"V"
-
-    # resistance = mean(abs.(in_voltages[1:length(out_currents)] ./ out_currents)) |> u"Ω"
-    # println("Resistance: $(resistance)")
 
     sweeps = parameters["daqiv"]["daqio"]["output"]["signal"]["regenerations"]
     quarter_sweep = div(length(out_samples), 4)
+    transition_threshold = 5.0u"mV"
 
     positive_offset = 0*quarter_sweep
     positive_indices = (1:quarter_sweep) .+ positive_offset
     positive_transitions = zeros(Int, quarter_sweep)
     positive_transition_currents = zeros(eltype(out_currents), sweeps)
     missing_positive_transitions = 0
+    positive_threshold_input_voltage = preamp_gain * ustrip(u"V", transition_threshold)
+    positive_threshold_sample = find_zero(s -> in_voltage(incoeff, s) - positive_threshold_input_voltage, 0)
 
     negative_offset = 2*quarter_sweep
     negative_indices = (1:quarter_sweep) .+ negative_offset
     negative_transitions = zeros(Int, quarter_sweep)
     negative_transition_currents = zeros(eltype(out_currents), sweeps)
     missing_negative_transitions = 0
+    negative_threshold_input_voltage = preamp_gain * ustrip(u"V", -transition_threshold)
+    negative_threshold_sample = find_zero(s -> in_voltage(incoeff, s) - negative_threshold_input_voltage, 0)
 
-    transition_threshold = 5.0u"mV"
     for sweep in 1:sweeps
         positive_sweep_indices = positive_indices .+ (sweep - 1) * length(out_samples)
-        positive_transition_index = findfirst(in_voltages[positive_sweep_indices] .> transition_threshold)
+        positive_transition_index = findfirst(in_samples[positive_sweep_indices] .> positive_threshold_sample)
         if isnothing(positive_transition_index)
             positive_transition_currents[sweep] = out_currents[1] # Sentinel value
             missing_positive_transitions += 1
@@ -96,7 +96,7 @@ function main(dn = 0, plot = true, savefig = true)
         end
         
         negative_sweep_indices = negative_indices .+ (sweep - 1) * length(out_samples)
-        negative_transition_index = findfirst(in_voltages[negative_sweep_indices] .< -transition_threshold)
+        negative_transition_index = findfirst(in_samples[negative_sweep_indices] .< negative_threshold_sample)
         if isnothing(negative_transition_index)
             negative_transition_currents[sweep] = out_currents[1] # Sentinel value
             missing_negative_transitions += 1
@@ -124,8 +124,7 @@ function main(dn = 0, plot = true, savefig = true)
         ax.title = "$(recent)"
 
         positive_kde = kde_lscv(ustrip.(u"μA", positive_transition_currents))
-        #positive_kde = kde(ustrip.(u"μA", positive_transition_currents), bandwidth = 1e-1)
-	lines!(ax, positive_kde.x, positive_kde.density)
+        lines!(ax, positive_kde.x, positive_kde.density)
         
         #=
         fig, ax, plt = lines(
